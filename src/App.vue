48d9e9d7fe9c8de658291cd823adcb9014d996b3
CodeTaco <daybreakfang@foxmail.com>
2020-10-08 21:23:07 +0800

    项目的模块划分：tabbar -> 路由映射关系

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    
+    <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
 
-
+import MainTabBar from 'components/content/MainTabBar'
 export default {
   name: 'app',
   components: {
-
+    MainTabBar
   }
 }
 </script>
---
+++ src/components/content/MainTabBar.vue
@@ -0,0 +1,39 @@
+<template>
+   <tab-bar>
+      <tab-bar-item path='/home' >
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home_fill.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path='/category' >
+        <img slot="item-icon" src="~assets/img/tabbar/goods_new_light.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/goods_new_fill_light.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path='/cart' >
+        <img slot="item-icon" src="~assets/img/tabbar/cart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/cart_fill.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path='/profile' >
+        <img slot="item-icon" src="~assets/img/tabbar/people.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/people_fill.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+import TabBar from "components/common/tabbar/TabBar";
+import TabBarItem from "components/common/tabbar/TabBarItem";
+export default {
+  components: {
+    TabBar,
+    TabBarItem
+  },
+ 
+}
+
+</script>
+<style  scoped>
+</style>
