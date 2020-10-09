e27efc182a7ef6a6a73bbfa1d6a03e994799a85e
CodeTaco <daybreakfang@foxmail.com>
2020-10-09 11:01:14 +0800

    首页添加导航栏

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,25 @@
 <template>
-  <div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
     <h2>首页</h2>
+
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-  
+  components:{
+    NavBar
+  }
 }
 
 </script>
 <style  scoped>
+.home-nav{
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
